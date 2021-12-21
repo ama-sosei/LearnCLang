df32937f694e1f86d9944c609aeb7cdd6a89995e
kurages <73342826+kurages@users.noreply.github.com>
2021-12-21 15:55:52 +0900

    first commit

---
+++ c_build.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal EnableDelayedExpansion
+pushd %~dp1
+
+del %~n1.err %~n1.exe > nul 2>&1
+
+gcc -finput-charset=UTF-8 -fexec-charset=CP932 -o %~n1.exe %1 2> %~n1.err
+
+if !errorlevel! == 0 (
+	type %~n1.err
+	%~n1.exe
+) else (
+	type %~n1.err
+)
+
+popd
+endlocal
+pause
+del %~n1.err %~n1.exe > nul 2>&1
+exit
